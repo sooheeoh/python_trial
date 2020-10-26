c7caded35e67255f8cdb83a0f5c0f3de73f5efb7
Soohee <sooheeoh@gmail.com>
2020-10-26 11:47:09 +0900

    모듈 학습에 관한 내용 업로드

---
+++ bmi_1017.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def clac_bmi(height, weight):
+    height = height * 0.01
+    return weight / (height **2 )
+
+
+# .py 하나가 모듈이다. .ipynb는 모듈로 활용할 수 없다.
+
+# In[ ]:
+
+
+
+
